5b8698148099294106d8b978aefbd20c118c7cce
Rafael Batista <rafaelb24@yahoo.com>
2019-03-22 20:44:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,20 +22,3 @@ class TitleValidator < ActiveModel::Validator
     end
   end 
 end 
-
-=begin 
-
-
-class MyValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.name.starts_with? 'X'
-      record.errors[:name] << 'Need a name starting with X please!'
-    end
-  end
-end
- 
-class Person
-  include ActiveModel::Validations
-  validates_with MyValidator
-end
-=end
