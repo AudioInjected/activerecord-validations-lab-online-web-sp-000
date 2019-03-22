e14abfb7365a05860b7324d0ad27850980854209
Rafael Batista <rafaelb24@yahoo.com>
2019-03-22 20:48:45 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,3 @@
-
-class TitleValidator < ActiveModel::Validator 
-  def validate(record) 
-    case record 
-    when record.title.include?("Won't Believe")
-      record.errors[:title] << 'Needs a ClickBait Title!' 
-    when record.title.include?("Secret")
-      record.errors[:title] << 'Needs a ClickBait Title!' 
-    when record.title.include?("Top [number]")
-      record.errors[:title] << 'Needs a ClickBait Title!' 
-    when record.title.include?("Guess")
-      record.errors[:title] << 'Needs a ClickBait Title!' 
-    end
-  end 
-end 
-
-
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates_with TitleValidator
