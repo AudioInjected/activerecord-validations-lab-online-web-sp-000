3ab3fdc0635488fc3048f574ad4b58afeda52c8e
Rafael Batista <rafaelb24@yahoo.com>
2019-03-22 21:43:42 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,6 @@ click_bait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
 
 def is_click_bait? 
   click_bait.each do |bait|
-     title.include?(bait) ? next : errors.add(:title, "Not Click Bait")
+     title.include?(bait) ? break : errors.add(:title, "Not Click Bait")
     end 
 end 
