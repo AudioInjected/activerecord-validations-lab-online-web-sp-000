80c101b0ed99c026d65fbfd42e1ee87e0d5ea7ee
Rafael Batista <rafaelb24@yahoo.com>
2019-03-22 21:11:34 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
 
- validates :title, presence: true 
+  validate :click_bait
+  validates :title, presence: true 
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
-  validate :click_bait
 end
 
 private 
