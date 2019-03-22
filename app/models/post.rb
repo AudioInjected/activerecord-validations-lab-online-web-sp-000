3a2d7362089d67d91978615bd7300f82c988eb8d
Rafael Batista <rafaelb24@yahoo.com>
2019-03-22 20:36:03 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,8 @@ end
 class TitleValidator < ActiveModel::EachValidator 
   def validate_each(record, attribute, value) 
     if !value.include?("Won't Believe") || !value.include?("Secret") || !value.include?("Top [number]") || !value.include?("Guess")
+      false 
+    end
   end 
 end 
 
